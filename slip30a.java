13d7d360ebe6df2fdba9b8280ad6aadb40eaf951
Shad0w35 <92471694+Shad0w35@users.noreply.github.com>
2023-05-11 08:28:37 +0530

    Update slip30a.java

--- slip30a.java
+++ slip30a.java
@@ -1,41 +1,48 @@
-class Table{  
- synchronized void printTable(int n){//synchronized method  
-   for(int i=1;i<=5;i++){  
-     System.out.println(n*i);  
-     try{  
-      Thread.sleep(400);  
-     }catch(Exception e){System.out.println(e);}  
-   }  
-  
- }  
-}  
-  
-class MyThread1 extends Thread{  
-Table t;  
-MyThread1(Table t){  
-this.t=t;  
-}  
-public void run(){  
-t.printTable(5);  
-}  
-  
-}  
-class MyThread2 extends Thread{  
-Table t;  
-MyThread2(Table t){  
-this.t=t;  
-}  
-public void run(){  
-t.printTable(100);  
-}  
-}  
-  
-public class Slip_A{  
-public static void main(String args[]){  
-Table obj = new Table();//only one object  
-MyThread1 t1=new MyThread1(obj);  
-MyThread2 t2=new MyThread2(obj);  
-t1.start();  
-t2.start();  
-}  
-}  
+class myThread extends Thread
+{
+    Job j;
+	
+    public myThread(Job j1, String str,int p) 
+    {    	
+    	super(str);
+    	System.out.println ("Started : " + str);
+    	this.j = j1;
+    	this.setPriority(p);
+    	start();
+    }
+    public void run()
+    {
+   		j.doJob1(Thread.currentThread().getName());
+    }
+}
+
+class Job
+{
+	public void doJob1(String str)
+	{
+		synchronized (this)
+		{
+			for(int i = 0 ; i < 5 ; i++)
+			{
+				System.out.println("Started : " + str);
+				try
+				{
+					Thread.sleep(1000);
+				}
+				catch(Exception e)
+				{}
+			}
+		}
+	}
+}
+
+public class Slip_A
+{
+	public static synchronized void main (String[] args) 
+	{
+		Job j = new Job();
+		myThread m1 = new myThread(j,"First",3);
+		myThread m2 = new myThread(j,"Second",4);
+		myThread m3 = new myThread(j,"Third",5);
+	}
+}
